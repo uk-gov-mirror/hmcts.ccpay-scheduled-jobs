185ebcaabab679c325c812fd876e2968e6b10c4a
Aravindan Ranjan <aravindan.ranjan@hmcts.net>
2019-07-26 16:41:44 +0100

    Gradle task for copying Jar added.

---
+++ App_Data/jobs/triggered/status-update/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export PATH=$JAVA_HOME/bin:$PATH
+function s2sToken() {
+    otp="$(java -cp $WEBROOT_PATH/payment-otp-all.jar uk.gov.hmcts.payment.otp.OtpBootstrap $WEBJOB_S2S_CLIENT_SECRET)"
+    s2s_token=$(curl -X POST $AUTH_PROVIDER_SERVICE_CLIENT_BASEURL/lease -H "Content-Type:application/json" -d "{\"microservice\":\"$WEBJOB_S2S_CLIENT_ID\",\"oneTimePassword\":\"$otp\"}")
+    echo $s2s_token
+    java -jar scheduled-jobs-1.0-all.jar "Bearer $s2s_token" "$PAYMENT_SERVER_URL" "status-update"
+}
