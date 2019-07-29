99f0d95cd3c3ed1eb26f89a10e8b5a1748f2fa27
Aravindan Ranjan <aravindan.ranjan@hmcts.net>
2019-07-29 11:57:34 +0100

    Refactoring changes.

---
+++ src/main/java/uk/gov/hmcts/s2s/S2STokenGeneration.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.s2s;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.jboss.aerogear.security.otp.Totp;
+
+
+public class S2STokenGeneration {
+    private Totp totp;
+    private S2sTokenService s2sTokenService = new S2sTokenService();
+    
+    public String generateOTP(String clientSecret, String authProviderServiceURL, String s2sClientId) throws Exception {
+
+        totp = new Totp(clientSecret);
+        String otp = totp.now();
+
+        System.out.println("Values---"+clientSecret+authProviderServiceURL+s2sClientId+otp);
+          Response response = RestAssured.given().relaxedHTTPSValidation()
+                .baseUri(authProviderServiceURL)
+                .contentType(ContentType.JSON)
+                .param("microservice", s2sClientId)
+                .param("oneTimePassword", otp)
+                .post("/lease");
+        System.out.println("Response Values---"+response.getBody().asString());
+        System.out.println("s2sTokenService---"+s2sTokenService);
+        //System.out.println("Values---"+s2sTokenService.getS2sToken(s2sClientId,s2s));
+        //String s2sToken = s2sTokenService.getS2sToken(s2sClientId,s2s);
+
+        //return s2sToken;
+        return response.getBody().asString();
+
+    }
+}
