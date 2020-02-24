dca6e8dcbdc57d4d9e296b75948afd9b3d727104
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2020-02-24 12:30:48 +0000

    PAY-2828:Added Webjob configuration for FPL

--- src/main/java/uk/gov/hmcts/payment/processors/PbaCsvReportProcessor.java
+++ src/main/java/uk/gov/hmcts/payment/processors/PbaCsvReportProcessor.java
@@ -5,10 +5,12 @@ import io.restassured.http.ContentType;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class PbaCsvReportProcessor implements JobProcessor {
 
     private final Map<String, String> headers = new HashMap<>();
+    private static final Logger LOG = Logger.getLogger(PbaCsvReportProcessor.class.getName());
 
     @Override
     public void process(String serviceToken, String baseURL) {
@@ -32,6 +34,8 @@ public class PbaCsvReportProcessor implements JobProcessor {
                 .contentType(ContentType.JSON)
                 .headers(headers)
                 .post(baseURL+"/jobs/email-pay-reports?payment_method=PBA&service_name=PROBATE");
+
+       LOG.info("Report is going to be generated for FPL");
 
         RestAssured.given().relaxedHTTPSValidation()
                 .contentType(ContentType.JSON)
