e80a3e7f0c7586d646597132c715617b9e810fc1
Tim Jacomb <tim.jacomb@hmcts.net>
2020-05-21 09:44:08 +0100

    getThrowsIfNotFound

--- src/test/java/uk/gov/hmcts/payment/processors/EnvironmentVariableRetrieverTest.java
+++ src/test/java/uk/gov/hmcts/payment/processors/EnvironmentVariableRetrieverTest.java
@@ -12,7 +12,14 @@ class EnvironmentVariableRetrieverTest {
     void getWithDefault() {
         EnvironmentVariableRetriever retriever = new EnvironmentVariableRetriever();
         
-        assertThat(retriever.get("ASDASDSADSA", "default"), 
-                is("default"));
+        assertThat(retriever.get("ASDASDSADSA", "default"), is("default"));
+    }
+
+    @Test
+    void getThrowsIfNotFound() {
+        EnvironmentVariableRetriever retriever = new EnvironmentVariableRetriever();
+
+        NullPointerException nullPointerException = assertThrows(NullPointerException.class, () -> retriever.get("ASDASDSADSA"));
+        assertThat(nullPointerException.getMessage(), is("ASDASDSADSA environment variable is required"));
     }
 }
