a1a285157e6948f50871393883374e08415b6984
ranjeet-kumarhmcts <ranjeet.kumar@hmcts.net>
2022-08-04 12:23:30 +0100

    unprocessed-payment-update job

---
+++ src/test/java/uk/gov/hmcts/payment/processors/UnprocessedPaymentUpdateProcessorTest.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.payment.processors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class UnprocessedPaymentUpdateProcessorTest extends BaseIntegrationTest {
+
+    @Test
+    void happyPathSucceeds() {
+        stubForS2s();
+        stubForCardStatusUpdate();
+
+        JobProcessorConfiguration configuration = new MockJobProcessorConfiguration(
+                "http://localhost:" + s2sWiremock.port(),
+                "http://localhost:" + payWiremock.port()
+        );
+
+        String s2sToken = new S2SHelper(configuration).generateToken();
+
+        UnprocessedPaymentUpdateProcessor processor = new UnprocessedPaymentUpdateProcessor();
+        assertDoesNotThrow(() -> processor.process(s2sToken, configuration.getPayUrl()));
+    }
+}
