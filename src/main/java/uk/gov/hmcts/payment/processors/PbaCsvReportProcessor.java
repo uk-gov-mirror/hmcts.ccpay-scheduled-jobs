c5666200bfbba167de36e7606ef51c0a5fa3bc10
“larslunde-hmcts” <lars.lunde@HMCTS.net>
2022-08-23 16:41:16 +0100

    removed duplicate code in favour of shared method

--- src/main/java/uk/gov/hmcts/payment/processors/PbaCsvReportProcessor.java
+++ src/main/java/uk/gov/hmcts/payment/processors/PbaCsvReportProcessor.java
@@ -24,52 +24,19 @@ public class PbaCsvReportProcessor implements JobProcessor {
         List<String> services = new ArrayList<>();
         services.add("Civil Money Claims");
         services.add("Divorce");
+        services.add("Finrem");
+        services.add("Probate");
+        services.add("Family Public Law");
+        services.add("Family Private Law");
+        services.add("civil_service");
+
 
         services.forEach((String service) -> {
             LOG.info(String.format("Report is going to be generated for %s", service));
             RestAssured.given().relaxedHTTPSValidation()
                     .contentType(ContentType.JSON)
                     .headers(headers)
-                    .post(baseURL+(String.format("/jobs/email-pay-reports?payment_method=PBA&service_name=%s", service));
+                    .post(baseURL+(String.format("/jobs/email-pay-reports?payment_method=PBA&service_name=%s", service)));
         });
-
-
-        LOG.info("Report is going to be generated for CMC");
-        // Need to replace service name, once CMC onboarded org id
-        RestAssured.given().relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .post(baseURL+"/jobs/email-pay-reports?payment_method=PBA&service_name=Civil Money Claims");
-
-        LOG.info("Report is going to be generated for Divorce");
-        RestAssured.given().relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .post(baseURL+"/jobs/email-pay-reports?payment_method=PBA&service_name=Divorce");
-
-        LOG.info("Report is going to be generated for Finrem");
-        // Need to replace service name, once Finrem onboarded org id
-        RestAssured.given().relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .post(baseURL+"/jobs/email-pay-reports?payment_method=PBA&service_name=Finrem");
-
-        LOG.info("Report is going to be generated for Probate");
-        RestAssured.given().relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .post(baseURL+"/jobs/email-pay-reports?payment_method=PBA&service_name=Probate");
-
-        LOG.info("Report is going to be generated for FPL");
-        RestAssured.given().relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .post(baseURL+"/jobs/email-pay-reports?payment_method=PBA&service_name=Family Public Law");
-
-        LOG.info("Report is going to be generated for Family Private Law");
-        RestAssured.given().relaxedHTTPSValidation()
-                .contentType(ContentType.JSON)
-                .headers(headers)
-                .post(baseURL+"/jobs/email-pay-reports?payment_method=PBA&service_name=Family Private Law");
     }
 }
