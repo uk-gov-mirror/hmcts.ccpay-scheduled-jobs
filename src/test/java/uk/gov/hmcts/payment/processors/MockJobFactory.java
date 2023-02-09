b95f1f3a10c3d462b759f3b6dff867137d6ad5ac
Pragnesh.Patel2 <94181713+PragneshPatel2@users.noreply.github.com>
2023-02-09 15:52:46 +0000

    Corrected job name

--- src/test/java/uk/gov/hmcts/payment/processors/MockJobFactory.java
+++ src/test/java/uk/gov/hmcts/payment/processors/MockJobFactory.java
@@ -4,14 +4,10 @@ import org.junit.jupiter.api.Test;
 
 public class MockJobFactory {
     JobProcessorFactory JobProcessorFactory = new JobProcessorFactory();
-    @Test
-    public void ewfund_approved(){
-         JobProcessorFactory.getJobType("refund-approved-update-process");
-    }
-
+ 
     @Test
     public void refund_notification(){
-        JobProcessorFactory.getJobType("refund-notification-update-process");
+        JobProcessorFactory.getJobType("refund-notifications-job");
     }
 
 }
