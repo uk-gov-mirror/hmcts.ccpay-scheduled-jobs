c5cc453d950a410ceb9cd24ef0255a1e69353a1b
Victoria Hulse <Victoria.Hulse@HMCTS.NET>
2023-05-02 15:22:53 +0100

    Test added for DuplicatPaymentProcessor

---
+++ src/test/java/uk/gov/hmcts/payment/processors/DuplicatePaymentProcessorTest.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.payment.processors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class DuplicatePaymentProcessorTest extends BaseIntegrationTest{
+
+    @Test
+    void happyPathSucceeds() {
+        stubForS2s();
+        stubForCardStatusUpdate();
+
+        JobProcessorConfiguration configuration = new MockJobProcessorConfiguration(
+                "http://localhost:" + s2sWiremock.port(),
+                "http://localhost:" + payWiremock.port()
+        );
+
+        String s2sToken = new S2SHelper(configuration).generateToken();
+
+        DuplicatePaymentProcessorTest processor = new DuplicatePaymentProcessorTest();
+        assertDoesNotThrow(() -> processor.process(s2sToken, configuration.getPayUrl()));
+    }
+}
+
